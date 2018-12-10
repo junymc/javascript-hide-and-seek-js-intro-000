5494e966e8844d30c59e64d1806e755062173bb3
Junyoung McArdle <juny.mcardle@gmail.com>
2018-12-10 19:13:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+ return document.querySelector('#grand-node div div div div')
+}
+
+// function increaseRankBy(n) {
+//   const lis = document
+//   .getElementById('app')
+//   .querySelectorAll('ul.ranked-list li');
+// 
+// for (let i = 0; i < lis.length; i++) {
+//   lis[i].innerHTML = (i + 1).toString();
+//  }
+// }
+  // const rankedLists = document.querySelectorAll('.ranked-list')
+  //
+  // for (let i = 0; i < rankedLists.length; i++) {
+    // let children = rankedLists[i].children
+    // for (let j = 0; j < children.length; j++) {
+    //   children[j].innerHTML += n
+    } return children
+  }
+}
